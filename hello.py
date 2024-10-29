49b1c10eb11722f98d8b44f84fa7483f968e9654
Mazal Mahari <who@Mazal-Mahari.local>
2024-10-29 15:56:34 +0200

    third_commit

--- hello.py
+++ hello.py
@@ -1,2 +1,27 @@
-print("hello I need PR please")
-print("now i try to send PR to mentor")
+import random
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+    print("Welcome to the 'Guess the Number' game!")
+    print("I am thinking of a number between 1 and 100.")
+    
+    while True:
+        try:
+            guess = int(input("Try to guess the number: "))
+            attempts += 1
+            
+            if guess < number_to_guess:
+                print("Your guess is too low. Try again!")
+            elif guess > number_to_guess:
+                print("Your guess is too high. Try again!")
+            else:
+                print(f"Correct! The number was {number_to_guess}.")
+                print(f"You finished the game in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    guess_the_number()
+
